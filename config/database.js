fa3f51737433782c11d7767d36f85457cdd74496
Akashpatil7596 <99664587+Akashpatil7596@users.noreply.github.com>
2023-01-31 13:26:30 +0530

    Add files via upload

--- config/database.js
+++ config/database.js
@@ -1,1 +1,17 @@
+const mysql = require('mysql');
 
+//Database Connection
+
+const dbConn = mysql.createConnection({
+    host  : 'localhost',
+    user  : 'root',
+    password  :  '',
+    database : 'crud_node_mysql'
+});
+
+dbConn.connect((err) => {
+    if (err) console.log('Error');;
+    console.log('Database Connection Successfull😍');
+});
+
+module.exports = dbConn;
